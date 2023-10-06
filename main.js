488767a6b8dea889e9e81ae379422848b1603c5c
dual-shock <siegemannsb@gmail.com>
2023-10-06 12:21:03 +0200

    testing

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ import {
     // * Firestore
     getFirestore, collection, 
     initializeFirestore,
+    persistentLocalCache,
     enableIndexedDbPersistence,
     CACHE_SIZE_UNLIMITED,
+    getDocsFromCache,
     onSnapshot,
     getDocs, setDoc, doc,
     orderBy, query, 
@@ -36,14 +38,12 @@ const firebaseConfig = {
 }
 const app = initializeApp(firebaseConfig);
 const db = initializeFirestore(app, {
-    cacheSizeBytes: CACHE_SIZE_UNLIMITED,
+    localCache: persistentLocalCache({
+        cacheSizeBytes:CACHE_SIZE_UNLIMITED
+    })
 })
 const auth = getAuth();
 
-enableIndexedDbPersistence(db)
-
-
-const inputElms =  [...grab('.login-element > input', "all")]
 
 const categories = {
     dream : {
@@ -60,13 +60,11 @@ const categories = {
     },
 }
 
-var timeframe = {
+const timeframe = {
     use:false,
     start:0,
     end:0,
 }
-
-var dataLoaded = false
 
 class Category {
     constructor(current){
@@ -94,13 +92,17 @@ class Category {
 
 const category = new Category(categories.diary)
 
-category.current = categories.dream
-category.current = categories.thought
-
 const months = [ 
     "January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December" 
 ]
+
+const inputElms =  [...grab('.login-element > input', "all")]
+
+const entriesElm = grab("entries")
+const sidebarElm = grab("sidebar")
+
+
 
 
 function switchToSignup(){
@@ -192,46 +194,109 @@ function resetAddEntryInputs(){
     //TODO 
 }
 
+
+// async function addEntry(
+//     
+//     categoryObj,
+//     unixSecs=Math.round(Date.now() / 1000),
+//     content="Lorem Ipsum"
+// ){
+//     console.log(categoryObj.current.name, unixSecs, content)
+//     if(1){
+//         try{
+//             let docRef = setDoc(doc(db, `users/${userId}/entries`, unixSecs), {
+//                 
+//                 category:categoryObj.current.name,
+//                 content:content
+//             })
+//         }
+//         catch(e){
+//             console.error("Error adding document: ", e);
+//         }
+//     }
+// }
+// await new Promise(r => setTimeout(r, 2000));
+// addEntry(category)
+// await new Promise(r => setTimeout(r, 2000));
+// addEntry(category)
+// await new Promise(r => setTimeout(r, 2000));
+// addEntry(category)
+// await new Promise(r => setTimeout(r, 2000));
+// addEntry(category)
+// await new Promise(r => setTimeout(r, 2000));
+// addEntry(category)
+
+// async function loadEntries(userId){
+//     //console.log("Load data")
+//     //TODO Add pagination, for now just load all then cache
+
+//     let queryCached = query(collection(db, `users/${userId}/entries`))
+    
+//     let cachedDocs = await getDocsFromCache(queryCached)
+
+//     if(cachedDocs.empty){
+//         let queryAll = query(collection(db, `users/${userId}/entries`))
+//         //getDocs(queryAll)
+//     }
+//     else{
+//         //TODO Check for missing indexes between last index and first index of cached docs
+
+//         // 
+//     }
+
+//     console.log(cachedDocs.empty)
+
+
+
+
+//     // onSnapshot(
+//     //     query(collection(db, `users/${userId}/entries`)), { includeMetadataChanges: true }, (snapshot) => {
+//     //         snapshot.docChanges().forEach((change) => {
+//     //             const source = snapshot.metadata.fromCache ? "local cache" : "server";
+//     //             if(change.type === "added"){
+//     //                 console.log("added:",change.doc.data(), "from", source)
+//     //             }
+//     //             if(change.type === "modified"){
+//     //                 console.log("modified:",change.doc.data(), "from", source)
+//     //             }
+//     //             if(change.type === "removed"){
+//     //                 console.log("removed:",change.doc.data(), "from", source)
+//     //             }
+//     //         })
+//     //     }
+//     // )
+// }
 async function loadEntries(userId){
-    //console.log("Load data")
-    //TODO Add pagination, for now just load all then cache
 
-    let q = query(collection(db, `users/${userId}/entries`))
-
-    var querySnapshot = await getDocs(q)
-
-
-    onSnapshot(
-        query(collection(db, `users/${userId}/entries`)), { includeMetadataChanges: false }, (snapshot) => {
-            snapshot.docChanges().forEach((change) => {
-                if(change.type === "added"){
-                    console.log("added:",change.doc.data())
-                }
-                if(change.type === "modified"){
-                    console.log("modified:",change.doc.data())
-                }
-                if(change.type === "removed"){
-                    console.log("removed:",change.doc.data())
-                }
-                console.log(change.type)
-                const source = snapshot.metadata.fromCache ? "local cache" : "server";
-                console.log("source:", source)
-            })
-        }
-    )
-
-
-    console.log(querySnapshot.docs[0].data())
-
-    dataLoaded = true
 }
+
 function filterEntries(categoryObj){
-    //console.log(grab("entries").childNodes)
+    //console.log(grab("entries").children)
     if(grab("entries").children.length){
-        //console.log("filter entries",categoryObj.current, timeframe)
+        console.log("filter entries",categoryObj.current, timeframe)
     }
     else{
         //console.log("no content to filter")
+    }
+}
+function reverseChildren(){
+    if(entriesElm.dataset.flipped == "false"){
+        entriesElm.style.flexDirection = "column-reverse";
+        entriesElm.style.justifyContent = "flex-end"
+        sidebarElm.style.flexDirection = "column-reverse";
+        sidebarElm.style.justifyContent = "flex-end"
+        entriesElm.dataset.flipped = "true"
+        grab("sidebar-toggle-button").innerHTML = "&#8593;"
+        return
+    }
+    if(entriesElm.dataset.flipped == "true"){
+        entriesElm.style.flexDirection = "column";
+        entriesElm.style.justifyContent = "flex-start"
+        sidebarElm.style.flexDirection = "column";
+        sidebarElm.style.justifyContent = "flex-start"
+        entriesElm.dataset.flipped = "false"
+        grab("sidebar-toggle-button").innerHTML = "&#8595;"
+        return
     }
 }
 function unloadEntries(){
@@ -253,9 +318,13 @@ function addEventListenersToElements(){
     grab("diary-selector-button").addEventListener("click", () => category.current = categories.diary)
     grab("thought-selector-button").addEventListener("click", () => category.current = categories.thought)
 
-    grab("logout-button").addEventListener("click", signOutUser)
+    grab("sidebar-toggle-button").addEventListener("click", reverseChildren)
     grab("add-entry-button").addEventListener("click", switchToAddEntry)
 
+    grab("logout-button").addEventListener("click", signOutUser)
+    
+
+// ? Add entry buttons
     grab("cancel-entry-button").addEventListener("click", switchToShowEntries)
 
     //TODO On add entry button, add a one time click event listener for loading drafts, so that theyre only read if theyre needed, and after that theyre saved in a variable
@@ -364,7 +433,23 @@ onAuthStateChanged(auth, async (user) => {
         resetAddEntryInputs()
         switchToShowEntries()
 
-        await loadEntries(user.uid)
+        //await loadEntries(user.uid)
+        onSnapshot(
+            query(collection(db, `users/${user.uid}/entries`)), { includeMetadataChanges: true }, (snapshot) => {
+                snapshot.docChanges().forEach((change) => {
+                    const source = snapshot.metadata.fromCache ? "local cache" : "server";
+                    if(change.type === "added"){
+                        console.log("added:",change.doc.data(), "from", source)
+                    }
+                    if(change.type === "modified"){
+                        console.log("modified:",change.doc.data(), "from", source)
+                    }
+                    if(change.type === "removed"){
+                        console.log("removed:",change.doc.data(), "from", source)
+                    }
+                })
+            }
+        )
     }
     else {
         console.log("hide content and show signin")
